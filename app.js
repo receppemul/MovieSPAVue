77b32258684443ed76f943fabf429af33925eae6
Recep Emul <recep971907@gmail.com>
2019-03-16 16:42:42 +0300

    HTTPS overload bug fixed

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var app = new Vue({
     },
     methods: {
         async search() {
-            const response = await fetch(`http://www.omdbapi.com/?t=${this.query}&apikey=25871e1f`)
+            const response = await fetch(`https://www.omdbapi.com/?t=${this.query}&apikey=25871e1f`)
             const responseJson = await response.json();
             this.movie = responseJson
         }
